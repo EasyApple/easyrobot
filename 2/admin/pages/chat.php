2f53c973097ad463b267bebe45fbabed00e7c2ae
Jack <jack@easyapple.net>
2013-03-08 06:58:13 +0000

    commit

--- 2/admin/pages/chat.php
+++ 2/admin/pages/chat.php
@@ -1,47 +1,32 @@
 <?PHP
 //-----------------------------------------------------------------------------------------------
-//Program-o Version 1.0.4
+//Program-o Version 1.0.1
 //PHP MYSQL AIML interpreter
 //Written by Elizabeth Perreau
 //Feb 2010
 //for more information and support please visit www.program-o.com
 //-----------------------------------------------------------------------------------------------
-include_once("bot/chat.php");
-
+//chat.php
+//contains the interface used to chat to the bot
+//-----------------------------------------------------------------------------------------------
+include("../../bot/chat.php");
+//-----------------------------------------------------------------------------------------------
+//Run program.. detect if form has been posted
+//If so run program
+//-----------------------------------------------------------------------------------------------
+$res = "";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Program-O AIML Chat Bot</title>
-<style type="text/css">
-<!--
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-	font-family:Verdana, Arial, Helvetica, sans-serif;
-	font-size:12px;
-}
-.demouser{
-	color:blue;
-}
-.demobot{
-	color:green;
-}
--->
-</style></head>
+<title>Bot Chat</title>
+</head>
 
 <body OnLoad="document.chat.chat.focus();">
-<div>
-  <p style=\"\">
-  <?php 
-  echo $res; 
-  echo "<br/>";
-  echo $formchat;
-  ?></p>
+<div id="Layer1">
+  <p style=\"\"><?php echo $res; echo $formchat;?></p>
 </div>
 <p>&nbsp;</p>
 </body>
