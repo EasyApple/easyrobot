7af5e9e8c1cc544066d87609cd42ff64ebc676dd
Jack <jack@easyapple.net>
2013-03-09 12:27:40 +0000

    Update

--- 1/admin/upload.php
+++ 1/admin/upload.php
@@ -127,8 +127,6 @@ endScript;
     //die('<pre>' . htmlentities("File contents:<br />\n$aimlFile"));
     try
     {
-      $msg = "$fn,$aimlContent<br />\n";
-
       libxml_use_internal_errors(true);
       $xml = new DOMDocument();
       $xml->loadXML($aimlFile);
@@ -213,7 +211,7 @@ endScript;
     catch (Exception $e)
     {
       $success = false;
-      //$msg = "There was a problem adding file $fileName to the database. Please validate the file and try again.<br >\n";
+      $msg = "There was a problem adding file $fileName to the database. Please validate the file and try again.<br >\n";
       $msg = libxml_display_errors($msg);
     }
     return $msg;
