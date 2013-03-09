aba19666a6861737fcb80e787049ab4817b67261
music_space@yahoo.cn <music_space@yahoo.cn@fce36a53-d45d-4ec3-91a4-8a6e1c14cbfb>
2013-03-09 13:59:57 +0000

    Update

--- 1/chatbot/conversation_start.php
+++ 1/chatbot/conversation_start.php
@@ -70,8 +70,7 @@
     //add any pre-processing addons
     $say = run_pre_input_addons($convoArr, $say);
     #die('say = ' . $say);
-    //Edit By Jack 20130309
-    //runDebug(__FILE__, __FUNCTION__, __LINE__, "Details:\nUser say: " . $_REQUEST['say'] . "\nConvo id: " . $_REQUEST['convo_id'] . "\nBot id: " . $_REQUEST['bot_id'] . "\nFormat: " . $_REQUEST['format'], 2);
+    runDebug(__FILE__, __FUNCTION__, __LINE__, "Details:\nUser say: " . $_REQUEST['say'] . "\nConvo id: " . $_REQUEST['convo_id'] . "\nBot id: " . $_REQUEST['bot_id'] . "\nFormat: " . $_REQUEST['format'], 2);
     //get the stored vars
     $convoArr = read_from_session();
     //now overwrite with the recieved data
@@ -99,7 +98,6 @@
       $convoArr['conversation']['totallines'] = 0;
       $convoArr = get_user_id($convoArr);
     }
-
     $convoArr['aiml'] = array();
     //add the latest thing the user said
     $convoArr = add_new_conversation_vars($say, $convoArr);
@@ -112,17 +110,20 @@
     $convoArr = run_post_response_useraddons($convoArr);
     //return the values to display
     $display = $convoArr['send_to_user'];
-    return;
-    //runDebug(__FILE__, __FUNCTION__, __LINE__, "Conversation Ending", 4);
+
+    //Added By Jack 20130309 
+    //Hide Debug Info
+    
+    runDebug(__FILE__, __FUNCTION__, __LINE__, "Conversation Ending", 4);
     $convoArr = handleDebug($convoArr);
-    //runDebug(__FILE__, __FUNCTION__, __LINE__, "Returning " . $convoArr['conversation']['format'], 4);
+    runDebug(__FILE__, __FUNCTION__, __LINE__, "Returning " . $convoArr['conversation']['format'], 4);
     if ($convoArr['conversation']['format'] == "html")
     {
     //TODO what if it is ajax call
       $time_start = $convoArr['time_start'];
       $time_end = microtime(true);
       $time = $time_end - $time_start;
-      //runDebug(__FILE__, __FUNCTION__, __LINE__, "Script took $time seconds", 2);
+      runDebug(__FILE__, __FUNCTION__, __LINE__, "Script took $time seconds", 2);
       return $convoArr['send_to_user'];
     }
     else
@@ -132,11 +133,11 @@
   }
   else
   {
-    //runDebug(__FILE__, __FUNCTION__, __LINE__, "Conversation intialised waiting user", 2);
+    runDebug(__FILE__, __FUNCTION__, __LINE__, "Conversation intialised waiting user", 2);
   }
-  //runDebug(__FILE__, __FUNCTION__, __LINE__, "Closing Database", 2);
+  runDebug(__FILE__, __FUNCTION__, __LINE__, "Closing Database", 2);
   $time_end = microtime(true);
   $time = $time_end - $time_start;
-  //runDebug(__FILE__, __FUNCTION__, __LINE__, "Script took $time seconds", 2);
+  runDebug(__FILE__, __FUNCTION__, __LINE__, "Script took $time seconds", 2);
 
 ?>
