0ff3416339f8d938241882d0718c630fde0ab6fa
music_space@yahoo.cn <music_space@yahoo.cn@fce36a53-d45d-4ec3-91a4-8a6e1c14cbfb>
2013-03-10 04:03:36 +0000

    Update

--- 1/gui/plain/index.php
+++ 1/gui/plain/index.php
@@ -42,7 +42,7 @@
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="CH" lang="CH" dir="ltr">
 	<head>
 		<link rel="icon" href="./favicon.ico" type="image/x-icon" />
 		<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
