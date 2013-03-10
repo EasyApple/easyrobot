f0dfacc4a86352783b6537c2681351ab4e37507b
music_space@yahoo.cn <music_space@yahoo.cn@fce36a53-d45d-4ec3-91a4-8a6e1c14cbfb>
2013-03-10 09:52:52 +0000

    Update

--- 1/chatbot/conversation_start.php
+++ 1/chatbot/conversation_start.php
@@ -111,7 +111,7 @@
     $convoArr = run_post_response_useraddons($convoArr);
     //return the values to display
     $display = $convoArr['send_to_user'];
-    echo $convoArr['send_to_user'];
+    //echo $convoArr['send_to_user'];
 
     //Added By Jack 20130309 
     //Hide Debug Info
