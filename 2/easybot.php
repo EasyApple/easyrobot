947b98356ce27d2f09a7f9d97257941259344c6f
music_space@yahoo.cn <music_space@yahoo.cn@fce36a53-d45d-4ec3-91a4-8a6e1c14cbfb>
2013-04-14 16:43:40 +0000

    Edit easybot.php

--- 2/easybot.php
+++ 2/easybot.php
@@ -5,18 +5,16 @@ class EasyRobot
 {
   public function reply($s)
   {
-    $display = "EasyRobot is being developed...";
+    $display = "";
     $thisFile = __FILE__;
     $say = $s;
     require_once ('config/global_config.php');
     require_once ('chatbot/conversation_start.php');
 
-    /*
     if(empty($display))
     {
       $display = "EasyRobot is being developed...";
     }
-    */
     return $display;
   }
 
