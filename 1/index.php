6c5e7c7d4b2e1288275f2d9dd6648124637456cc
Jack <jack@easyapple.net>
2016-03-21 15:22:47 +0000

    auto versioning

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,28 @@
 <?php
-echo '<strong>Hello, SAE!</strong>';
+
+  if (!file_exists("config/global_config.php"))
+  {
+  # No config exists we will run install
+    header("location: install");
+  }
+  else
+  {
+    # Config exists we will goto the bot
+    $thisFile = __FILE__;
+    require_once('config/global_config.php');
+    $default_format = strtoupper($default_format);
+    switch ($default_format)
+    {
+      case 'JSON':
+      $gui = 'jquery';
+      break;
+      case 'XML':
+      $gui= 'xml';
+      break;
+      default:
+      $gui = 'plain';
+    }
+    header("location: gui/$gui");
+  }
+
+?>
